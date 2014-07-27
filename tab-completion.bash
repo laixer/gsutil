d75d52803c7a56d01271dbbcfada5b239644dd70
Vadim Berezniker <vadim@berezniker.com>
2014-07-26 22:56:36 -0400

    Prefer parenthesis to backticks.

--- tab-completion.bash
+++ tab-completion.bash
@@ -4,11 +4,11 @@ _gsutil_completer()
     COMPREPLY=()
     _get_comp_words_by_ref -n : cur cword
     if [[ $cword -eq 1 ]]; then
-        COMPREPLY=(`gsutil help | sed /Additional/q | grep '^  ' | sed -e 's/^  //' -e 's/ .*//'`)
+        COMPREPLY=($(gsutil help | sed /Additional/q | grep '^  ' | sed -e 's/^  //' -e 's/ .*//'))
     elif [[ "$cur" =~ ^gs://.* ]]; then
-        COMPREPLY=(`gsutil ls ${cur}*`)
+        COMPREPLY=($(gsutil ls ${cur}*))
     else
-        COMPREPLY=(`compgen -A file ${cur}`)
+        COMPREPLY=($(compgen -A file ${cur}))
     fi
     __ltrim_colon_completions "$cur"
     return 0
