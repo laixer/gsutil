c1d1e14d23249327b1042cbfde38a38e18d995c0
Vadim Berezniker <vadim@berezniker.com>
2014-07-26 22:54:56 -0400

    Allow local file completion.

--- tab-completion.bash
+++ tab-completion.bash
@@ -1,16 +1,18 @@
-_gsutil()
+_gsutil_completer()
 {
     local cur cword
     COMPREPLY=()
     _get_comp_words_by_ref -n : cur cword
     if [[ $cword -eq 1 ]]; then
         COMPREPLY=(`gsutil help | sed /Additional/q | grep '^  ' | sed -e 's/^  //' -e 's/ .*//'`)
-    else
+    elif [[ "$cur" =~ ^gs://.* ]]; then
         COMPREPLY=(`gsutil ls ${cur}*`)
+    else
+        COMPREPLY=(`compgen -A file ${cur}`)
     fi
     __ltrim_colon_completions "$cur"
     return 0
 }
 
-complete -F _gsutil -o dirnames gsutil
+complete -F _gsutil_completer -o dirnames gsutil
 
