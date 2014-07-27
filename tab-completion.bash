88e89c52f57ea2a6cc4d4915acd4febc2aa74677
Vadim Berezniker <vadim@berezniker.com>
2014-07-26 22:48:46 -0400

    Use 4 space indent.

--- tab-completion.bash
+++ tab-completion.bash
@@ -1,15 +1,15 @@
 _gsutil()
 {
-  local cur cword
-  COMPREPLY=()
-  _get_comp_words_by_ref -n : cur cword
-  if [[ $cword -eq 1 ]]; then
-    COMPREPLY=(`gsutil help | sed /Additional/q | grep '^  ' | sed -e 's/^  //' -e 's/ .*//'`)
-  else
-    COMPREPLY=(`gsutil ls ${cur}*`)
-  fi
-  __ltrim_colon_completions "$cur"
-  return 0
+    local cur cword
+    COMPREPLY=()
+    _get_comp_words_by_ref -n : cur cword
+    if [[ $cword -eq 1 ]]; then
+        COMPREPLY=(`gsutil help | sed /Additional/q | grep '^  ' | sed -e 's/^  //' -e 's/ .*//'`)
+    else
+        COMPREPLY=(`gsutil ls ${cur}*`)
+    fi
+    __ltrim_colon_completions "$cur"
+    return 0
 }
 
 complete -F _gsutil -o dirnames gsutil
